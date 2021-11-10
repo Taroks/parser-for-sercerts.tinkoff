997c7998574d363c275088cfb8eb06a62928d1ba
Polukhin.Artemiy <kanavskij@yandex.ru>
2021-11-10 12:53:00 +0300

    добавлена функция подгрузки авторов, не решена проблема дублирования авторов в словаре

--- output.py
+++ output.py
@@ -4,15 +4,16 @@ import pandas as pd
 
 class preparing_to_output:
     def __init__(self):
-        self.analys = Analytic()
-        self.result_of_comparison = self.analys.comparison()
         self.parser = Views()
         self.links = self.parser.links()
         self.views = self.parser.views()
         self.authors = self.parser.author()
+        self.analys = Analytic()
+        self.result_of_comparison = self.analys.comparison()
         self.links_and_views = {}
-        self.authors_and_views = {}
-        self.final_count = {}
+        self.authors_and_views = []
+        self.final_count_links = {}
+        self.final_count_authors = {}
 
 
     def type_of_outcome_links(self):
@@ -20,31 +21,49 @@ class preparing_to_output:
             self.links_and_views[link] = view
         return self.links_and_views
 
-    def type_of_outcome_autrhors(self):
-        for author, view in list(zip(self.authors,self.views)):
-            self.authors_and_views[author] = view
+    def type_of_outcome_authors(self):
+        authors = []
+        views = []
+        a = dict(zip(self.views, self.authors))
+        print (len(self.authors), len(self.views), len(a))
+        # for author, view in list(zip(self.authors,self.views)):
+        #     print (author, ":", view)
+        #     authors.append(author)
+        #     views.append(view)
+        #     print (len(authors), len(views))
+        for i,j in list(zip(authors,views)):
+                self.authors_and_views.append(list.append(i,j))
+        print(self.authors_and_views)
+        # print (self.authors_and_views)
         return self.authors_and_views        
 
-    def outcome(self):
+    def outcome_links(self):
         for key in self.type_of_outcome_links():
             if self.links_and_views[key] in self.result_of_comparison:
                 value = self.links_and_views.get(key)
-                self.final_count[key] = value
-        return self.final_count
+                self.final_count_links[key] = value
+        return self.final_count_links
+
+    def outcome_authors(self):
+        for key in self.type_of_outcome_authors():
+            if self.authors_and_views[key] in self.result_of_comparison:
+                value = self.authors_and_views.get(key)
+                self.final_count_authors[key] = value
+        return self.final_count_authors
+        
 
     def output(self):
         i = 0
         list_of_links = []
         list_of_views = []
-        for key in self.outcome():
-            # i += 1
-            # line_for_output:str = str(key) + ' ; ' + 'количество просмотров: ' + str(self.final_count[key])
-            # self.file.write("номер " + str(i) + ' ссылка: ' +
-            #  line_for_output + '\n')
+        list_of_authors = []
+        for key in self.outcome_links():
             list_of_links.append(key)
-            list_of_views.append(self.final_count[key])
-        df = pd.DataFrame({'links':list_of_links, 'views':list_of_views})
-        df.to_excel('analys.xlsx', index = False)
+            list_of_views.append(self.final_count_links[key])
+        for key in self.outcome_authors():
+            list_of_authors.append(key)
+        # df = pd.DataFrame({'authors':list_of_authors, 'links':list_of_links, 'views':list_of_views})
+        # df.to_excel('analys.xlsx', index = False)
 
 check = preparing_to_output()
 check.output()
--- parser.py
+++ parser.py
@@ -3,8 +3,6 @@ from bs4 import BeautifulSoup
 from lxml import html
 import re
 import time
-import logging
-
 
 class Views:
     def __init__(self):
@@ -13,10 +11,23 @@ class Views:
         self.list_of_views = []
         self.list_of_authors = []
 
+    def connection (self,link):
+        while True:
+            try:
+                request = requests.get(link)
+                break
+            except Exception as e:
+                time.sleep(5)
+                continue
+        return request
+
+
     def links (self):
+        print ( '_'* 70,'function links','_'*70)
         if len(self.list_of_links) == 0:
             for page in self.pages():
-                req = requests.get(page).text
+                r = self.connection(page)
+                req = r.text
                 soup = BeautifulSoup(req, 'lxml')
                 for link in soup.main.section.find_all('a'):
                     rigth_links = re.findall(r'novosti', str(link))
@@ -25,6 +36,7 @@ class Views:
         return self.list_of_links
 
     def pages (self):
+        print ( '_'* 70,'function pages','_'*70)
         num = 1
         req = requests.get('https://secrets.tinkoff.ru/novosti/page/2/').text
         soup = BeautifulSoup(req,'lxml')
@@ -38,37 +50,26 @@ class Views:
         return self.list_of_pages
         
     def views(self):
-        counter: int = 0
+        print ( '_'* 70,'function views','_'*70)
         for link in self.links():
-            while True:
-                try:
-                    r = requests.get(link)
-                    if r.status_code != 200:
-                        logging.info("Ошибка, Код ответа: %s", r.status)
-                        time.sleep(1)
-                        continue
-                    req = r.text
-                    soup = BeautifulSoup(req,'lxml')
-                    count_of_views = str(soup.find('span', {'class': "post-views-count"}))
-                    result = re.findall(r'\d+', count_of_views)
-                    if len(result) != 0:
-                        if len(result) < 2:
-                            self.list_of_views.append(int(result[0]))
-                        else:
-                            self.list_of_views.append(int(result[0] + result[1]))
-                    counter += 1
-                    print('!'*20, "counter:", counter, '!'*20, r.status_code)
-                    if counter == len(self.list_of_links):
-                        break
-                except Exception as e:
-                    print(type(e), '\nstatus code: ',r.status_code)
-                    time.sleep(1)
+            r = self.connection(link)
+            req = r.text
+            soup = BeautifulSoup(req,'lxml')
+            count_of_views = str(soup.find('span', {'class': "post-views-count"}))
+            result = re.findall(r'\d+', count_of_views)
+            if len(result) != 0:
+                if len(result) < 2:
+                    self.list_of_views.append(int(result[0]))
+                else:
+                    self.list_of_views.append(int(result[0] + result[1]))
         return self.list_of_views
 
     def author(self):
+        print ( '_'* 70,'function author','_'*70)
         a = 0
         for link in self.list_of_links:
-            req = requests.get(link).text
+            r = self.connection(link)
+            req = r.text
             soup = BeautifulSoup(req,'lxml')
             author_str = str(soup.find('span', {'class': "details__editor"}))
             left_author = re.split(r'>', author_str)
@@ -82,5 +83,6 @@ class Views:
 
 
 # check = Views()
-# check.views()
-# print(check.views())
+# links = check.views()
+# authors = check.author()
+# print(links, '\n', len(links), "\n", authors, len(authors))
